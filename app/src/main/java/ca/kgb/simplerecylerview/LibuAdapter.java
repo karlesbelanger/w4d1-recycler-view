7b42ff3d2c198bc257966c03d0718dae9bf19bf4
karlesbelanger <karlesbelanger@gmail.com>
2016-08-08 23:24:37 -0400

    adding snackbar dependencies

--- app/src/main/java/ca/kgb/simplerecylerview/LibuAdapter.java
+++ app/src/main/java/ca/kgb/simplerecylerview/LibuAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,13 +19,37 @@ import java.util.ArrayList;
  */
 public class LibuAdapter extends RecyclerView.Adapter<LibuAdapter.ViewHolder> {
 
+    String[] mDataSet = {"Data"};
     private ArrayList<String> mString;
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public TextView textViewName;
+        public IMyViewHolderClicks mListener;
 
-        public ViewHolder(View itemView) {
+        //pass my listener in param
+        public ViewHolder(View itemView, IMyViewHolderClicks listener) {
             super(itemView);
+            mListener = listener;
+            //Set up the listener on each item and the view
             textViewName = (TextView)itemView.findViewById(R.id.r_list_text);
+            textViewName.setOnClickListener(this);
+            itemView.setOnClickListener(this);
+
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            if(view instanceof TextView){
+                mListener.onTextViewListener(view);
+                Snackbar.make(findViewById(android.R.id.content), "Snackbar chez Raymon", Snackbar.LENGTH_LONG)
+                        .setAction("Undo", mListener)
+                        .setActionTextColor(Color.RED)
+                        .show();
+            }
+        }
+        //create interface listener for my objects
+        public static interface IMyViewHolderClicks{
+            public void onTextViewListener(View caller);
         }
     }
 
@@ -36,7 +61,16 @@ public class LibuAdapter extends RecyclerView.Adapter<LibuAdapter.ViewHolder> {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view = layoutInflater.inflate(R.layout.list_item, parent, false);
 
-        return new ViewHolder(view);
+        LibuAdapter.ViewHolder viewHolder = new ViewHolder(view, new ViewHolder.IMyViewHolderClicks() {
+            public final String TAG = LibuAdapter.class.getSimpleName();
+
+            @Override
+            public void onTextViewListener(View caller) {
+                Log.d(TAG, "onTextViewListener: " + "We are in my recycle view listener");
+            }
+        });
+
+        return viewHolder;
     }
 
     @Override
@@ -45,7 +79,23 @@ public class LibuAdapter extends RecyclerView.Adapter<LibuAdapter.ViewHolder> {
 
         TextView textView = holder.textViewName;
         textView.setText(string);
+
+        //holder.textViewName.setText(mDataSet[position]);
     }
+//        ///////////////////////////////////////////
+//        final String element = mDataset[position];
+//
+//        holder.itemView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                onClickSubject.onNext(element);
+//            }
+//        });
+//    }
+//
+//    public Observable<String> getPositionClicks(){
+//        return onClickSubject.asObservable();
+//    }
 
     @Override
     public int getItemCount() {
